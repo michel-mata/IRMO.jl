38e4b638e01efe19152d250c3163c3370df34429
Documenter.jl <documenter@juliadocs.github.io>
2023-06-29 08:22:58 +0000

    build based on d5a0d11

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#IRMO.jl","page":"Home","title":"IRMO.jl","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for IRMO.jl","category":"page"}]
+}
