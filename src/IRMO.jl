64ab88d7becc4b3088952198c64f193e5eb17fd1
michel-mata <sm75@princeton.edu>
2023-08-30 10:59:27 -0400

    add norm competition

--- src/IRMO.jl
+++ src/IRMO.jl
@@ -10,19 +10,26 @@ module IRMO
     using Distributed
     using SharedArrays
 
-    # Objects:
     include("./module/functions.jl")
+    # Auxiliar functions
     export chop_pt
     export get_norm
     export get_rep
     export get_initial_frequencies
     export get_vertex, get_eigenvalues
+    # Strategy competition
     export get_full_dynamics
     export get_time_dynamics
     export steady_states_detIC
     export steady_states_rndIC
     export trajectories_detIC
     export refine_steady_states
+    # Norm competition
+    export get_full_dynamics_n
+    export get_time_dynamics_n
+    export steady_states_detIC_n
+    export steady_states_rndIC_n
+    export refine_steady_states_n
 
     # Auxiliary constants:
     export social_norms
